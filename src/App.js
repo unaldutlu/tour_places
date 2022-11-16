3bc422cbb563827715799b51c689f9d5d1a79142
unaldutlu <unaldutlu@gmail.com>
2022-11-16 18:05:15 +0300

    add header-navbar-tour folders on page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Tour from "./components/Tour";
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello</h1>
+      <Navbar />
+      <Header />
+      <Tour />
     </div>
   );
 }
---
+++ src/components/Tour.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Tour() {
+  return (
+    <div>
+      <h1>Tour</h1>
+    </div>
+  );
+}
+
+export default Tour;
